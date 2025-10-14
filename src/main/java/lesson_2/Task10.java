65bf3aee2d6cdef8127dcb0a355488b4dc96f380
chalaeva <chalaeva0@yandex.ru>
2025-10-14 20:49:18 +0300

    Create Task10.java

---
+++ src/main/java/lesson_2/Task10.java
@@ -0,0 +1,14 @@
+public class Task10 {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
+            }
+            System.out.print(arr[i] + " ");
+        }
+    }
+}

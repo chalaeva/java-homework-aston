28316cd5f36468d84f96b0a531ecdf4f79ce8a2d
chalaeva <chalaeva0@yandex.ru>
2025-10-14 20:48:06 +0300

    Create Task7.java

---
+++ src/main/java/lesson_2/Task7.java
@@ -0,0 +1,9 @@
+public class Task7 {
+    public static void main(String[] args) {
+        System.out.println(isNegative(-7));
+    }
+
+    static boolean isNegative(int x) {
+        return x < 0;
+    }
+}

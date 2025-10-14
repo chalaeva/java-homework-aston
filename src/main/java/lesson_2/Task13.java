bb499eeedb1e60ecad2368126084474efc9882df
chalaeva <chalaeva0@yandex.ru>
2025-10-14 20:50:32 +0300

    Create Task13.java

---
+++ src/main/java/lesson_2/Task13.java
@@ -0,0 +1,17 @@
+public class Task13 {
+    public static void main(String[] args) {
+        int n = 5;
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i][i] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}

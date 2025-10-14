7216fe3afb1612081d2c66073e5f19d5d92a7878
chalaeva <chalaeva0@yandex.ru>
2025-10-14 20:41:31 +0300

    Create Task2.java

---
+++ src/main/java/lesson_2/Task2.java
@@ -0,0 +1,13 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = -8;
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+}
